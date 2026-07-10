e685078220763e44188a869bf4c67f88b5fe007c
Pallavi <141386088+SinghPallavi819@users.noreply.github.com>
2026-07-10 11:05:29 -0500

    Update view_source.php

--- vulnerabilities/view_source.php
+++ vulnerabilities/view_source.php
@@ -12,7 +12,7 @@ if (array_key_exists ("id", $_GET) && array_key_exists ("security", $_GET)) {
 	$id       = $_GET[ 'id' ];
 	$security = $_GET[ 'security' ];
 
-// this is a test
+// this is a test!!
 	switch ($id) {
 		case "fi" :
 			$vuln = 'File Inclusion';
